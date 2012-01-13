214f7e0d992bf105ce941fb5a163bebc0e79d635
Rogan Shimmin <rogan.shimmin@adelaide.edu.au>
2012-01-13 16:13:34 +1030

    Converted images for Propagate, Capture, Descent.

---
+++ Images/Ascent/myConverter.bat
@@ -0,0 +1,34 @@
+ps2pdf figure1.eps
+ps2pdf figure2.eps
+ps2pdf figure3.eps
+ps2pdf figure4.eps
+ps2pdf figure5.eps
+ps2pdf figure6.eps
+ps2pdf figure7.eps
+ps2pdf figure8.eps
+ps2pdf figure9.eps
+ps2pdf figure10.eps
+ps2pdf figure11.eps
+ps2pdf figure12.eps
+ps2pdf figure13.eps
+ps2pdf figure14.eps
+ps2pdf figure15.eps
+ps2pdf figure16.eps
+
+
+inkscape figure1.eps.pdf -z -D --export-pdf=figure1.pdf --export-latex
+inkscape figure2.eps.pdf -z -D --export-pdf=figure2.pdf --export-latex
+inkscape figure3.eps.pdf -z -D --export-pdf=figure3.pdf --export-latex
+inkscape figure4.eps.pdf -z -D --export-pdf=figure4.pdf --export-latex
+inkscape figure5.eps.pdf -z -D --export-pdf=figure5.pdf --export-latex
+inkscape figure6.eps.pdf -z -D --export-pdf=figure6.pdf --export-latex
+inkscape figure7.eps.pdf -z -D --export-pdf=figure7.pdf --export-latex
+inkscape figure8.eps.pdf -z -D --export-pdf=figure8.pdf --export-latex
+inkscape figure9.eps.pdf -z -D --export-pdf=figure9.pdf --export-latex
+inkscape figure10.eps.pdf -z -D --export-pdf=figure10.pdf --export-latex
+inkscape figure11.eps.pdf -z -D --export-pdf=figure11.pdf --export-latex
+inkscape figure12.eps.pdf -z -D --export-pdf=figure12.pdf --export-latex
+inkscape figure13.eps.pdf -z -D --export-pdf=figure13.pdf --export-latex
+inkscape figure14.eps.pdf -z -D --export-pdf=figure14.pdf --export-latex
+inkscape figure15.eps.pdf -z -D --export-pdf=figure15.pdf --export-latex
+inkscape figure16.eps.pdf -z -D --export-pdf=figure16.pdf --export-latex
